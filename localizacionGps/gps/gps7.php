12637c3a18099bae795a366f4714f5b0d4bb5aaf
beerpizzacode1000 <alexander.fernandez.arango@gmail.com>
2017-03-27 14:35:06 -0500

    otra k

--- localizacionGps/gps/gps7.php
+++ localizacionGps/gps/gps7.php
@@ -8,7 +8,7 @@ $f = fopen($file,"w");
 //fwrite($f, date("Y-m-d H:i:s")."_".$_GET["lat"]."_".$_GET["lon"]);
 fputs($f,date("Y-m-d H:i:s")."_".$_GET["lat"]."_".$_GET["lon"]);
 fclose($f);
-    echo "OKK";
+    echo "OKKk";
 } else {
     echo 'Please type this URL in the <a href="https://play.google.com/store/apps/details?id=fr.herverenault.selfhostedgpstracker">Self-Hosted GPS Tracker</a> Android app on your phone.';
 }
